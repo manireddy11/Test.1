ad8ee542c4c9053832f87cbf7b2e6557db64f63e
manireddy11 <reddy71042@gmail.com>
2024-09-27 14:48:10 +0530

    Add files via upload

---
+++ README (1).md
@@ -0,0 +1,2 @@
+# test1
+test1 repo git command demonstration
---
+++ hungry.py
@@ -0,0 +1,6 @@
+hungry=input("are you hungry")
+if hungry =="yes":
+    print("eat a biriyani")
+    print("eat pizza")
+else:
+    print("do your work")
